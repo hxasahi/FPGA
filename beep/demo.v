02bc09b80ae3c87a07ba0c98d4bda81b85a9f58a
hxasahi <3287039936@qq.com>
2023-04-29 23:11:16 +0800

    分析不是按键抖动导致逻辑错误

--- beep/demo.v
+++ beep/demo.v
@@ -5,6 +5,7 @@ output reg beep=1'b0;
 output reg[4:0] led;
 reg[31:0] cnt;
 reg[31:0] freq = 32'd100_000;
+//蜂鸣器
 always @ (posedge clk)
 begin
 	cnt<=cnt+1;
@@ -14,6 +15,7 @@ begin
 		beep<=~beep;
 	end
 end
+//判断按键功能
 always @ (posedge clk)
 begin
 	case(key)
@@ -24,6 +26,14 @@ begin
 		~5'b10000:freq<=32'd500_000;
 		default:freq<=32'd600_000;
 	endcase
-	led <= key;
+	case(freq)
+		32'd100_000:led=5'b00001;
+		32'd200_000:led=5'b00011;
+		32'd300_000:led=5'b00111;
+		32'd400_000:led=5'b01111;
+		32'd500_000:led=5'b11111;
+		32'd600_000:led=5'b00000;
+		default:led=5'b10101;
+	endcase
 end
 endmodule
